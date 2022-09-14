983bb29b58bed2ea0a7247d0bce5f557dfe5f761
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-09-14 15:49:25 -0400

    edited functions for html

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateHTML = require('./utils/generateHTML');
+//const generateHTML = require('./utils/generateHTML');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
+const { getEventListeners } = require('events');
 
 // Array where the entered team info will be stored
-let generatedTeam = [];
+let teamData = [];
 
 const managerQuestions = [
     {
@@ -95,7 +96,7 @@ const internQuestions = [
     }
 ]
 
-// inital questions when starting the app
+// Menu after manager has been added
 const menuQuestions = [
     {
         type: "list",
@@ -105,22 +106,17 @@ const menuQuestions = [
     }
 ]
 
-// function to write index.html
-function writeToFile(newFile, answers) {
-    fs.writeFileSync(newFile, answers, (err) => err ? console.error(err) : console.log('Success!'));
-}
-
 // function to initialize app 
 function init() {
     inquirer.prompt(managerQuestions)
-        .then((managerData) => {
+        .then((res) => {
             const manager = new Manager(
-                managerData.name,
-                managerData.id,
-                managerData.email,
-                managerData.officeNumber
+                res.name,
+                res.id,
+                res.email,
+                res.officeNumber
             );
-            generatedTeam.push(manager);
+            teamData.push(manager);
             addTeam();
     })
 }
@@ -135,39 +131,128 @@ function addTeam() {
                 case "Add an Intern":
                     return addIntern();
                 case "Done adding team members":
-                    console.log(generatedTeam);
-                    return generateHTML(generatedTeam);
-            } 
+                    console.log(teamData);
+                    generateHTML(teamData);
+            }
         })
 }
 
 
 function addEngineer() {
     inquirer.prompt(engineerQuestions)
-        .then((engineerData) => {
+        .then((res) => {
             const engineer = new Engineer(
-                engineerData.name,
-                engineerData.id,
-                engineerData.email,
-                engineerData.github
+                res.name,
+                res.id,
+                res.email,
+                res.github
             );
-            generatedTeam.push(engineer)
+            teamData.push(engineer)
             addTeam();
         })
 }
 
 function addIntern() {
     inquirer.prompt(internQuestions)
-        .then((internData) => {
+        .then((res) => {
             const intern = new Intern(
-                internData.name,
-                internData.id,
-                internData.email,
-                internData.school
+                res.name,
+                res.id,
+                res.email,
+                res.school
             );
-            generatedTeam.push(intern)
+            teamData.push(intern)
             addTeam();
         })
+}
+
+// Creates the profile cards to be inserted into HTML
+function createCards(team) {
+    let cardHTML = '';
+    for(let i = 0; i < teamData.length; i++)  {
+        if (team[i].getRole() === 'Manager'){
+            cardHTML += `
+                <div class="card m-3" style="width: 18rem">
+                        <div class="card-header bg-primary text-white">
+                            <h3>${team[i].getName()}</h3>
+                            <span class="h5"><i class="bi bi-cup-hot-fill"></i> Manager</span>
+                        </div>
+                        <div class="card-body">
+                            <ul class ="list-group">
+                                <li class="list-group-item">ID: ${team[i].getID()} </li>
+                                <li class="list-group-item">Email: ${team[i].getEmail()} </li>
+                                <li class="list-group-item">Office number: ${team[i].getOfficeNumber()}</li>
+                            </ul>
+                        </div>
+                    </div>`
+        } else if (team[i].getRole() === 'Engineer'){
+            cardHTML += `
+                <div class="card m-3" style="width: 18rem">
+                        <div class="card-header bg-primary text-white">
+                            <h3>${team[i].getName()}</h3>
+                            <span class="h5"><i class="bi bi-wrench-adjustable-circle"></i> Engineer</span>
+                        </div>
+                        <div class="card-body">
+                            <ul class ="list-group">
+                                <li class="list-group-item">ID: ${team[i].getID()}</li>
+                                <li class="list-group-item">Email: ${team[i].getEmail()} </li>
+                                <li class="list-group-item">GitHub: ${team[i].getGitHub()} </li>
+                            </ul>
+                        </div>
+                    </div>`
+        } else if (team[i].getRole() === 'Intern'){
+            cardHTML += `
+                <div class="card m-3" style="width: 18rem">
+                        <div class="card-header bg-primary text-white">
+                            <h3>${team[i].getName()} </h3>
+                            <span class="h5"><i class="bi bi-wrench-adjustable-circle"></i> Engineer</span>
+                        </div>
+                        <div class="card-body">
+                            <ul class ="list-group">
+                                <li class="list-group-item">ID: ${team[i].getID()} </li>
+                                <li class="list-group-item">Email: ${team[i].getEmail()} </li>
+                                <li class="list-group-item">School: ${team[i].getSchool()} </li>
+                            </ul>
+                        </div>
+                    </div>`
+        }
+    return cardHTML;
+    };
+};
+
+function generateHTML(team){
+   let indexHTML = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="./style.css"">
+        <title>Team Members</title>
+    </head>
+    <body>
+        <div class="jumbotron jumbotron-fluid bg-danger text-white">
+            <div class="container">
+                <h1 class="display-4 text-center  font-weight-bold">TEAM MEMBERS</h1>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row justify-content-center" id="insert-cards">
+               ${createCards(team)}
+        </div>
+    </body>
+    </html>
+    `
+    return writeToFile(indexHTML);
+}
+
+
+// Function to write index.html
+function writeToFile(data) {
+    fs.writeFileSync('index.html', data, (err) => err ? console.error(err) : console.log('Successfully created index.html!!'));
 }
 
 
--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -1,8 +1,34 @@
-function generateHTML(data){
-    let mainHTML = `
+
+// function generateHTML(createCards){
+//     return `
+// <!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//         <meta charset="UTF-8">
+//         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+//         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+//         <link rel="stylesheet" href="./style.css"">
+//         <title>Team Members</title>
+//     </head>
+//     <body>
+//         <div class="jumbotron jumbotron-fluid bg-danger text-white">
+//             <div class="container">
+//                 <h1 class="display-4 text-center  font-weight-bold">TEAM MEMBERS</h1>
+//             </div>
+//         </div>
+
+//         <div class="container">
+//             <div class="row justify-content-center" id="insert-cards">
+//                ${createCards()}
+//         </div>
+        
     
-    `
-}
+//     </body>
+//     </html>
+//     `
+// }
 
 
-module.exports = generateHTML;
+//module.exports = generateHTML;
